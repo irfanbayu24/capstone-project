a8e66a74b7a308b7d793bc5a1263e76fa3a56c48
irfanbayu24 <bayu3cool@gmail.com>
2024-06-05 07:42:11 +0700

    Fourth commit

---
+++ login_sistem.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+# Importing the main_page function from page_1.py
+from main.app import main_page
+
+# Function to check credentials
+def check_credentials(username, password):
+    correct_username = "admin"  # Replace with actual username
+    correct_password = "admin"  # Replace with actual password
+    return username == correct_username and password == correct_password
+
+# Function for login form
+def login():
+    st.title("Login Page")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if check_credentials(username, password):
+            st.session_state['logged_in'] = True
+            st.experimental_rerun()
+        else:
+            st.error("Invalid username or password")
+
+# Main function
+def main():
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+
+    if not st.session_state['logged_in']:
+        login()
+    else:
+        main_page()
+
+if __name__ == "__main__":
+    main()
---
+++ main/app.py
@@ -0,0 +1,43 @@
+import pickle
+import streamlit as st
+
+#membaca model
+fraud_model = pickle.load(open('model_rfc.sav', 'rb'))
+
+#judul web
+def main_page():
+    st.title('Fraud Transaction Detection Simple App')
+
+    col1, col2 = st.columns(2)
+
+    with col1 :
+        step = st.selectbox('Step', [1])
+    with col2 :
+        amount = st.number_input('Amount', min_value=0.0, max_value=10000000.0)
+    with col1 :
+        isFraud = st.selectbox('Fraud', [0, 1])
+    with col2 :
+        isFlaggedFraud = st.selectbox('Flagged Fraud', [0, 1])
+    with col1 :
+        oldbalanceOrg = st.number_input('Old Balance Original', min_value=0.0, max_value=10000000.0)
+    with col2 :
+        newbalanceOrig = st.number_input('New Balance Original', min_value=0.0, max_value=10000000.0)
+    with col1 :
+        oldbalanceDest = st.number_input('Old Balance Destination', min_value=0.0, max_value=10000000.0)
+    with col2 :
+        newbalanceDest = st.number_input('New Balance Destination', min_value=0.0, max_value=10000000.0)
+
+
+    #code untuk prediksi
+    detect_fraud = ''
+
+    #tombol untuk memprediksi
+    if st.button('Predict Fraud Transaction'):
+        fraud_prediction = fraud_model.predict([[step, amount, isFraud, isFlaggedFraud, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest]])
+
+        if fraud_prediction[0] == 1:
+            detect_fraud = st.success('Transaction is not fraud')
+        else:
+            detect_fraud = st.error('Transaction is fraud')
+
+    
