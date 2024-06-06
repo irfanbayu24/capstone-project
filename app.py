24ca5f4e022ffd4656e4eb7750ea664ff6c91d3f
irfanbayu24 <bayu3cool@gmail.com>
2024-06-06 20:52:12 +0700

    penambahan fitur translate

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from translate import Translator
 with open('model_rfc.sav', 'rb') as model_file:
     fraud_model = pickle.load(model_file)
 
-#fungsi untuk menerjemahkan teks
+#fungsi untuk menerjemahkan teks dengan caching
+@st.cache_data
 def translate_text(text, dest_language):
     translator = Translator(to_lang=dest_language)
     translation = translator.translate(text)
@@ -19,7 +20,7 @@ def main_page():
 
     languages = {
         'English': 'en',
-        'Indonesian': 'id',
+        'Indonesian': 'id'
     }
 
     selected_language = st.radio('Select Language', list(languages.keys()))
@@ -29,18 +30,18 @@ def main_page():
 
     with col1:
         step = st.selectbox(translate_text('Step', dest_language), [1])
-    with col2:
-        amount = st.number_input(translate_text('Amount', dest_language), min_value=0.0, max_value=10000000.0)
     with col1:
         isFraud = st.selectbox(translate_text('Fraud', dest_language), [0, 1])
-    with col2:
-        isFlaggedFraud = st.selectbox(translate_text('Flagged Fraud', dest_language), [0, 1])
     with col1:
         oldbalanceOrg = st.number_input(translate_text('Old Balance Original', dest_language), min_value=0.0, max_value=10000000.0)
-    with col2:
-        newbalanceOrig = st.number_input(translate_text('New Balance Original', dest_language), min_value=0.0, max_value=10000000.0)
     with col1:
         oldbalanceDest = st.number_input(translate_text('Old Balance Destination', dest_language), min_value=0.0, max_value=10000000.0)
+    with col2:
+        amount = st.number_input(translate_text('Amount', dest_language), min_value=0.0, max_value=10000000.0)
+    with col2:
+        isFlaggedFraud = st.selectbox(translate_text('Flagged Fraud', dest_language), [0, 1])
+    with col2:
+        newbalanceOrig = st.number_input(translate_text('New Balance Original', dest_language), min_value=0.0, max_value=10000000.0)
     with col2:
         newbalanceDest = st.number_input(translate_text('New Balance Destination', dest_language), min_value=0.0, max_value=10000000.0)
 
